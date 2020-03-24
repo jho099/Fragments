140f9512ee598e489a4b75adad1f545c990b664e
jenniferho <jennifersho09@gmail.com>
2020-03-24 16:08:52 -0400

    Initial commit

---
+++ app/src/main/java/com/hojennifer/fragments/frag_1.java
@@ -0,0 +1,14 @@
+package com.hojennifer.fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class frag_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frag_1);
+    }
+}
